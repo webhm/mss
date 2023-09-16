0a4a492b628d617b646257cd2822a8b1ad945020
martinxapps <52510496+martinxapps@users.noreply.github.com>
2023-09-16 18:42:23 -0500

    dev

--- src/models/ApiHTTP.js
+++ src/models/ApiHTTP.js
@@ -1,5 +1,13 @@
 class ApiHTTP {
     static apiUrl = '/api';
+    constructor() {
+        if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
+            ApiHTTP.apiUrl = '/api';
+        } else {
+            ApiHTTP.apiUrl = 'https://mss.hospitalmetropolitano.org/api';
+        }
+
+    }
 }
 
 export default ApiHTTP;
